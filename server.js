f5406045a4f72b1bf21c6c97efeb4ad4c9196886
karrde00 <karrde00@gmail.com>
2014-08-05 22:46:01 -0500

    adds mongoose and mongodb

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
+var mongoose = require('mongoose');
+
 http.createServer(function(req,res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, world\n');
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://localhost:3000');
+
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log('Database connected!')
+});
