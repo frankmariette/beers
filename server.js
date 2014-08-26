c2560bb5a908674ec0ed77a8e6bc17caa6d36b08
karrde00 <karrde00@gmail.com>
2014-08-26 08:09:18 -0500

    removes server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req,res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, world\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://localhost:3000');
